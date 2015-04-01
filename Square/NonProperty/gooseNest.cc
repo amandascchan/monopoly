93020b7e1b3ae0bee9fcbbcc154ac56b2126264d
tmhdunn <travisdunn@rogers.com>
2015-04-01 12:27:33 -0400

    work

--- Square/NonProperty/gooseNest.cc
+++
@@ -1,6 +0,0 @@
-#include "gooseNest.h"	
-#include "NPInclude.h.gch"
-
-GooseNest::GooseNest(Board *theBoard, TextDisplay *td):
-		Square(theBoard, td), name("Goose Nest"){}
-void GooseNest::action(){}
--- Square/NonProperty/nonproperty.cc
+++
@@ -1,2 +0,0 @@
-#include "nonproperty.h"
-NonProperty::~NonProperty(){}
