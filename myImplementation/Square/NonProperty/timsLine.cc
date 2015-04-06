2bd6aaa3ec140c5336d493677b582ec94adba878
amandascchan <amandascchan@gmail.com>
2015-04-06 02:46:35 -0400

    docs

--- myImplementation/Square/NonProperty/timsLine.cc
+++
@@ -1,6 +0,0 @@
-#include "timsLine.h"
-void TimsLine::action(){}
-
-TimsLine::TimsLine(Board *theBoard, Square *prevSquare, Square *nextSquare):
-					theBoard(theBoard), prevSquare(prevSquare),
-					nextSquare(nextSquare), name("DC Tims Line"){}
